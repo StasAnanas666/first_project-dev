e1ca82c79cd3fb40c2cccf5e8c60d3cfb7abaf5e
StasAnanas666 <lapshin-5a@yandex.ru>
2024-05-12 19:08:43 +0300

    add useState for example

--- src/pages/Main/About/About.jsx
+++ src/pages/Main/About/About.jsx
@@ -1,5 +1,21 @@
+import {useState} from "react";
+
 const About = () => {
-    return <h2 className="text-center">About page</h2>
+    //count - состояние
+    //setCount - функция для изменения состояния
+    //внутри useState передается начальное значение состояния
+    const [count, setCount] = useState(0);
+    function handleClick() {
+        setCount(count + 1)
+    }
+
+    return (
+        <div className="container">
+            <h2 className="text-center">About page</h2>
+            <p>Count: {count}</p>
+            <button onClick={handleClick}>Увеличить</button>
+        </div>
+    )
 }
 
 export default About;
--- src/pages/Main/Contacts/Contacts.jsx
+++ src/pages/Main/Contacts/Contacts.jsx
@@ -1,5 +1,26 @@
+import {useState} from "react";
+
 const Contacts = () => {
-    return <h2 className="text-center">Contacts page</h2>
+    const[name, setName] = useState("");
+    const[email, setEmail] = useState("");
+
+    return (
+        <div className="container">
+            <h2 className="text-center mb-5">Contacts page</h2>
+
+            <div className="form-group">
+                <label className="form-label">Имя: </label>
+                <input type="text" className="form-control" value={name} onChange={(event) => setName(event.target.value)}/>
+            </div>
+            <div className="form-group">
+                <label className="form-label">Почта: </label>
+                <input type="email" className="form-control" value={email} onChange={(event) => setEmail(event.target.value)}/>
+            </div>
+
+            <p className="mt-5">Имя: {name}</p>
+            <p>Почта: {email}</p>
+        </div>
+    )
 }
 
 export default Contacts;
